c182af6ed1fc8aacfd9c623650c24e601d273ea7
Alex <a37488269@gmail.com>
2022-10-29 05:54:43 +1200

    Comments about numbers

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@ namespace C
     static void Main() {
       
    int number;
+   // byte 255<=
+   // short 33000<
+   // float num = 1234.04958f (Дробные)
+   // double num2 = 5.324d
+
+
    number = 600;
-   Console.WriteLine(number);
+   Console.WriteLine("Переменная: " + number + " Верно?");
 
     }
 } 
