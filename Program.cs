b1fee9bcdd49158f8b33f4961bdcdf888c93dbec
Alex <a37488269@gmail.com>
2022-10-30 05:18:16 +1200

    Radius + Math.

--- Program.cs
+++ Program.cs
@@ -6,24 +6,30 @@ namespace C
     static void Main() {
       
  
-   // byte 255<=
+   // byte 255<=using System;
+        
+
+
+{
+            //System.Console.WriteLine(Math.PI);
+
+
+            //user_input = Convert.ToDouble(Console.ReadLine());
    // short 33000<
    // float num = 1234.04958f (Дробные)
    // double num2 = 5.324d
    // string 
    // Bool - true or false
 
-    int num_1 = 0 , num_2 = 0 ;
+   System.Console.WriteLine("Введите радиус круга : ");
+   double radius = Convert.ToDouble(System.Console.ReadLine());
 
-      num_1 = Convert.ToInt32(Console.ReadLine());
-      num_2 = Convert.ToInt32(Console.ReadLine());
-
-      Console.WriteLine ("First: " + num_1 + ". Second: "+ num_2 );
-
+    double area = Math.PI * Math.Pow(radius , 2);
+    System.Console.WriteLine("Площадь круга с радиусом {0} равна {1}", radius , area);
 
 
     }
 } 
 
     
-}
+}}
